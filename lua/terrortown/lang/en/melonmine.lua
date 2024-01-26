298d5a771d4b23effa4b71ad043046c4e52c00e9
Tim Goll <github@timgoll.de>
2024-01-26 12:06:46 +0100

    fixed lang identifier

--- lua/terrortown/lang/en/melonmine.lua
+++ lua/terrortown/lang/en/melonmine.lua
@@ -3,4 +3,4 @@ local L = LANG.GetLanguageTableReference("en")
 L["weapon_melonmine_name"] = "Melon Mine"
 L["weapon_melonmine_desc"] = "Stand close to a wall to plant the mine. Detonates when enemy is within visible range."
 L["weapon_melonmine_destroyed"] = "Your mine has been destroyed!"
-L["springmine_help_pri"] = "Stick Melon Mine on surface"
+L["melonmine_help_pri"] = "Stick Melon Mine on surface"
