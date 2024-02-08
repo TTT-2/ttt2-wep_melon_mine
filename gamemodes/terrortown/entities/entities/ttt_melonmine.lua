c4755a0e222797559a8e330330c97f7b6222fc16
Tim Goll <github@timgoll.de>
2024-02-08 02:08:35 +0100

    stylua; small cleanup

--- gamemodes/terrortown/entities/entities/ttt_melonmine.lua
+++ gamemodes/terrortown/entities/entities/ttt_melonmine.lua
@@ -1,7 +1,7 @@
 if SERVER then
-	AddCSLuaFile()
+    AddCSLuaFile()
 
-	util.PrecacheSound("npc/scanner/combat_scan2.wav")
+    util.PrecacheSound("npc/scanner/combat_scan2.wav")
 end
 
 AccessorFunc(ENT, "dmg", "Dmg", FORCE_NUMBER)
@@ -9,217 +9,226 @@ AccessorFunc(ENT, "dmg", "Dmg", FORCE_NUMBER)
 ENT.Base = "ttt_base_placeable"
 
 if CLIENT then
-	ENT.PrintName = "weapon_melonmine_name"
-	ENT.Icon = "vgui/ttt/icon_melonmine.png"
+    ENT.PrintName = "weapon_melonmine_name"
+    ENT.Icon = "vgui/ttt/icon_melonmine.png"
 end
 
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
 
-ENT.Projectile = true
-ENT.CanHavePrints = true
 ENT.CanUseKey = true
+ENT.pickupWeaponClass = "weapon_ttt_melonmine"
 
-ENT.WarningSound = Sound("weapons/c4/c4_beep1.wav")
+ENT.soundWarning = Sound("weapons/c4/c4_beep1.wav")
 
 function ENT:Initialize()
-	self:SetModel("models/props_junk/watermelon01.mdl")
-	self:SetColor(Color(255, 175, 0, 255))
+    self:SetModel("models/props_junk/watermelon01.mdl")
+    self:SetColor(Color(255, 175, 0, 255))
 
-	self.BaseClass.Initialize(self)
+    self.BaseClass.Initialize(self)
 
-	self:SetHealth(100)
+    self:SetHealth(100)
 
-	if SERVER then
-		self:WeldToSurface(true)
+    if SERVER then
+        self:WeldToSurface(true)
 
-		self.StartupDelay = CurTime() + 3
-		self.fingerprints = {}
+        self.StartupDelay = CurTime() + 3
+        self.fingerprints = {}
 
-		if not self:GetDmg() then
-			self:SetDmg(200)
-		end
+        if not self:GetDmg() then
+            self:SetDmg(200)
+        end
 
-		self:SetUseType(SIMPLE_USE)
+        local mvObject = self:AddMarkerVision("melon_owner")
+        mvObject:SetOwner(self:GetOriginator())
+        mvObject:SetVisibleFor(VISIBLE_FOR_TEAM)
+        mvObject:SyncToClients()
+    end
 
-		local mvObject = self:AddMarkerVision("melon_owner")
-		mvObject:SetOwner(self:GetOriginator())
-		mvObject:SetVisibleFor(VISIBLE_FOR_TEAM)
-		mvObject:SyncToClients()
-	end
+    if CLIENT then
+        self.DrawText = false
+        self.Text = ""
 
-	if CLIENT then
-		self.DrawText = false
-		self.Text = ""
+        local rand = math.random(1, 3)
+        if rand == 1 then
+            self.Text = "O_O"
+        elseif rand == 2 then
+            self.Text = "O.o"
+        else
+            self.Text = "-.-"
+        end
 
-		local rand = math.random(1,3)
-		if rand == 1 then
-			self.Text = "O_O"
-		elseif rand == 2 then
-			self.Text = "O.o"
-		else
-			self.Text = "-.-"
-		end
-
-		timer.Simple(3, function()
-			if IsValid(self) then
-				self.DrawText = true
-			end
-		end)
-	end
-end
-
-function ENT:UseOverride(activator)
-	if not IsValid(activator) or self:GetOriginator() ~= activator then return end
-
-	activator:GiveEquipmentWeapon("weapon_ttt_melonmine")
-
-	self:Remove()
+        timer.Simple(3, function()
+            if IsValid(self) then
+                self.DrawText = true
+            end
+        end)
+    end
 end
 
 if CLIENT then
-	surface.CreateFont("TrebuchetMelon", { font = "Trebuchet24", size = 60 })
+    surface.CreateFont("TrebuchetMelon", { font = "Tahoma", size = 60, antialias = true })
 end
 
 function ENT:Draw()
-	self:DrawModel()
+    self:DrawModel()
 
-	local FixAngles = self:GetAngles()
-	local FixRotation = Vector(0, 270, 0)
+    local FixAngles = self:GetAngles()
+    local FixRotation = Vector(0, 270, 0)
 
-	FixAngles:RotateAroundAxis(FixAngles:Right(), FixRotation.x)
-	FixAngles:RotateAroundAxis(FixAngles:Up(), FixRotation.y)
-	FixAngles:RotateAroundAxis(FixAngles:Forward(), FixRotation.z)
+    FixAngles:RotateAroundAxis(FixAngles:Right(), FixRotation.x)
+    FixAngles:RotateAroundAxis(FixAngles:Up(), FixRotation.y)
+    FixAngles:RotateAroundAxis(FixAngles:Forward(), FixRotation.z)
 
-	local TargetPos = self:GetPos() + (self:GetUp() * 9)
-	local _,_,_,a = self:GetColor()
+    local TargetPos = self:GetPos() + (self:GetUp() * 9)
+    local _, _, _, a = self:GetColor()
 
-	if self.DrawText then
-		cam.Start3D2D(TargetPos, FixAngles, 0.15)
-		draw.SimpleText(self.Text, "TrebuchetMelon", TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, Color(255, 0, 0, a), 1, 1)
-		cam.End3D2D()
-	end
+    if self.DrawText then
+        cam.Start3D2D(TargetPos, FixAngles, 0.15)
+        draw.SimpleText(
+            self.Text,
+            "TrebuchetMelon",
+            TEXT_ALIGN_CENTER,
+            TEXT_ALIGN_CENTER,
+            Color(255, 0, 0, a),
+            1,
+            1
+        )
+        cam.End3D2D()
+    end
 end
 
 ENT.StartupDelay = nil
 
 if SERVER then
-	function ENT:OnRemoe()
-		self:RemoveMarkerVision("melon_owner")
-	end
+    function ENT:OnRemoe()
+        self:RemoveMarkerVision("melon_owner")
+    end
 
-	function ENT:Think()
-		if not self.StartupDelay or self.StartupDelay >= CurTime() then return end
+    function ENT:Think()
+        if not self.StartupDelay or self.StartupDelay >= CurTime() then
+            return
+        end
 
-		local foundEnts = ents.FindInSphere(self:GetPos(), 150)
+        local foundEnts = ents.FindInSphere(self:GetPos(), 150)
 
-		for i = 1, #foundEnts do
-			local ply = foundEnts[i]
+        for i = 1, #foundEnts do
+            local ply = foundEnts[i]
 
-			if not IsValid(ply) or not ply:IsPlayer()
-				or ply:GetTeam() == self:GetOriginator():GetTeam()
-				or (isfunction(ply.IsGhost) and ply:IsGhost())
-			then continue end
+            if
+                not IsValid(ply)
+                or not ply:IsPlayer()
+                or ply:GetTeam() == self:GetOriginator():GetTeam()
+                or (isfunction(ply.IsGhost) and ply:IsGhost())
+            then
+                continue
+            end
 
-			local rags = ents.FindByClass("prop_ragdoll")
-			local trace = {}
+            local rags = ents.FindByClass("prop_ragdoll")
+            local trace = {}
 
-			table.insert(rags, self)
+            table.insert(rags, self)
 
-			trace.start = self:GetPos()
-			trace.endpos = ply:GetPos() + Vector(0, 0, 60)
-			trace.filter = rags
+            trace.start = self:GetPos()
+            trace.endpos = ply:GetPos() + Vector(0, 0, 60)
+            trace.filter = rags
 
-			local tr = util.TraceLine(trace)
+            local tr = util.TraceLine(trace)
 
-			-- Checks if there's a clear view of the player, the melon can be hidden beneath a ragdoll
-			if IsValid(tr.Entity) and tr.Entity == ply then
-				timer.Create("beep", 0.1, 8, function()
-					if not IsValid(self) then return end
+            -- Checks if there's a clear view of the player, the melon can be hidden beneath a ragdoll
+            if IsValid(tr.Entity) and tr.Entity == ply then
+                timer.Create("beep", 0.1, 8, function()
+                    if not IsValid(self) then
+                        return
+                    end
 
-					self:EmitSound("weapons/c4/c4_beep1.wav")
-				end)
+                    self:EmitSound(soundWarning)
+                end)
 
-				timer.Simple(1, function()
-					if not IsValid(self) then return end
+                timer.Simple(1, function()
+                    if not IsValid(self) then
+                        return
+                    end
 
-					self:Explode(ply)
-				end)
+                    self:Explode(ply)
+                end)
 
-				function self.Think()
+                function self.Think() end
+            end
+        end
+    end
 
-				end
-			end
-		end
-	end
+    function ENT:Explode(plyTrigger)
+        self:SetNoDraw(true)
+        self:SetSolid(SOLID_NONE)
 
-	function ENT:Explode(plyTrigger)
-		self:SetNoDraw(true)
-		self:SetSolid(SOLID_NONE)
+        local pos = self:GetPos()
 
-		local pos = self:GetPos()
+        if self:GetPos().z < plyTrigger:GetPos().z + 50 then
+            pos.z = pos.z + 30
+        end
 
-		if self:GetPos().z < plyTrigger:GetPos().z + 50 then
-			pos.z = pos.z + 30
-		end
+        local dmgOwner = self:GetOriginator()
+        dmgOwner = IsValid(dmgOwner) and dmgOwner or self
 
-		local dmgOwner = self:GetOriginator()
-		dmgOwner = IsValid(dmgOwner) and dmgOwner or self
+        local r_outer = 240
 
-		local r_outer = 240
+        -- explosion damage
+        util.BlastDamage(self, dmgOwner, pos, r_outer, self:GetDmg())
 
-		-- explosion damage
-		util.BlastDamage(self, dmgOwner, pos, r_outer, self:GetDmg())
+        sound.Play("explode_4", self:GetPos(), 130, 100)
 
-		sound.Play("explode_4", self:GetPos(), 130, 100)
+        local effect = EffectData()
+        effect:SetStart(pos)
+        effect:SetOrigin(pos)
+        effect:SetScale(r_outer)
+        effect:SetRadius(r_outer)
+        effect:SetMagnitude(self:GetDmg())
 
-		local effect = EffectData()
-		effect:SetStart(pos)
-		effect:SetOrigin(pos)
-		effect:SetScale(r_outer)
-		effect:SetRadius(r_outer)
-		effect:SetMagnitude(self:GetDmg())
+        effect:SetOrigin(pos)
+        util.Effect("Explosion", effect, true, true)
+        util.Effect("HelicopterMegaBomb", effect, true, true)
 
-		effect:SetOrigin(pos)
-		util.Effect("Explosion", effect, true, true)
-		util.Effect("HelicopterMegaBomb", effect, true, true)
+        -- extra push
+        local phexp = ents.Create("env_physexplosion")
+        phexp:SetPos(pos)
+        phexp:SetKeyValue("magnitude", self:GetDmg())
+        phexp:SetKeyValue("radius", r_outer)
+        phexp:SetKeyValue("spawnflags", "19")
+        phexp:Spawn()
+        phexp:Fire("Explode", "", 0)
 
-		-- extra push
-		local phexp = ents.Create("env_physexplosion")
-		phexp:SetPos(pos)
-		phexp:SetKeyValue("magnitude", self:GetDmg())
-		phexp:SetKeyValue("radius", r_outer)
-		phexp:SetKeyValue("spawnflags", "19")
-		phexp:Spawn()
-		phexp:Fire("Explode", "", 0)
+        self:Remove()
+    end
 
-		self:Remove()
-	end
+    local smallExplosion = 75
 
-	local smallExplosion = 75
+    function ENT:WasDestroyed(pos, dmgInfo)
+        local originator = self:GetOriginator()
+        local attacker = dmgInfo:GetAttacker()
 
-	function ENT:WasDestroyed(pos, dmgInfo)
-		local originator = self:GetOriginator()
-		local attacker = dmgInfo:GetAttacker()
+        local blastAttacker = (IsValid(attacker) and attacker:IsPlayer()) and attacker or originator
 
-		local blastAttacker = (IsValid(attacker) and attacker:IsPlayer()) and attacker or originator
+        util.BlastDamage(self, blastAttacker or self, pos, smallExplosion, smallExplosion)
 
-		util.BlastDamage(self, blastAttacker or self, pos, smallExplosion, smallExplosion)
+        local effect = EffectData()
+        effect:SetStart(pos)
+        effect:SetOrigin(pos)
+        effect:SetScale(smallExplosion)
+        effect:SetRadius(smallExplosion)
+        effect:SetMagnitude(smallExplosion)
 
-		local effect = EffectData()
-		effect:SetStart(pos)
-		effect:SetOrigin(pos)
-		effect:SetScale(smallExplosion)
-		effect:SetRadius(smallExplosion)
-		effect:SetMagnitude(smallExplosion)
+        effect:SetOrigin(pos)
+        util.Effect("Explosion", effect, true, true)
 
-		effect:SetOrigin(pos)
-		util.Effect("Explosion", effect, true, true)
+        LANG.Msg(originator, "weapon_melonmine_destroyed", nil, MSG_MSTACK_WARN)
 
-		LANG.Msg(originator, "weapon_melonmine_destroyed", nil, MSG_MSTACK_WARN)
+        return "Scorch"
+    end
 
-		return "Scorch"
-	end
+    function ENT:PlayerCanPickupWeapon(activator)
+        return self:GetOriginator() == activator
+    end
 end
 
 -- It seems intuitive to use FindInSphere here, but that will find all ents
@@ -227,93 +236,103 @@ end
 -- efficient to cycle through all those players and do a Lua-side distance
 -- check.
 function ENT:SphereDamage(dmgOwner, center, radius)
-	local r = radius ^ 2 -- square so we can compare with dotproduct directly
+    local r = radius ^ 2 -- square so we can compare with dotproduct directly
 
-	-- pre-declare to avoid realloc
-	local d = 0.0
-	local diff = nil
-	local dmg = 0
+    -- pre-declare to avoid realloc
+    local d = 0.0
+    local diff = nil
+    local dmg = 0
 
-	local plys = player.GetAll()
+    local plys = player.GetAll()
 
-	for i = 1, #plys do
-		local ply = plys[i]
+    for i = 1, #plys do
+        local ply = plys[i]
 
-		if not IsValid(ply) or not ply:IsTerror() then continue end
+        if not IsValid(ply) or not ply:IsTerror() then
+            continue
+        end
 
-		-- dot of the difference with itself is distance squared
-		diff = center - ply:GetPos()
-		d = diff:Dot(diff)
+        -- dot of the difference with itself is distance squared
+        diff = center - ply:GetPos()
+        d = diff:Dot(diff)
 
-		if d < r then
-			-- deadly up to a certain range, then a quick falloff within 100 units
-			d = math.max(0, math.sqrt(d) - 490)
-			dmg = -0.01 * (d ^ 2) + 125
+        if d < r then
+            -- deadly up to a certain range, then a quick falloff within 100 units
+            d = math.max(0, math.sqrt(d) - 490)
+            dmg = -0.01 * (d ^ 2) + 125
 
-			local dmginfo = DamageInfo()
-			dmginfo:SetDamage(dmg)
-			dmginfo:SetAttacker(dmgOwner)
-			dmginfo:SetInflictor(self)
-			dmginfo:SetDamageType(DMG_BLAST)
-			dmginfo:SetDamageForce(center - ply:GetPos())
-			dmginfo:SetDamagePosition(ply:GetPos())
+            local dmginfo = DamageInfo()
+            dmginfo:SetDamage(dmg)
+            dmginfo:SetAttacker(dmgOwner)
+            dmginfo:SetInflictor(self)
+            dmginfo:SetDamageType(DMG_BLAST)
+            dmginfo:SetDamageForce(center - ply:GetPos())
+            dmginfo:SetDamagePosition(ply:GetPos())
 
-			ply:TakeDamageInfo(dmginfo)
-		end
-	end
+            ply:TakeDamageInfo(dmginfo)
+        end
+    end
 end
 
 if CLIENT then
-	local TryT = LANG.TryTranslation
-	local ParT = LANG.GetParamTranslation
+    local TryT = LANG.TryTranslation
+    local ParT = LANG.GetParamTranslation
 
-	local materialMelonmine = Material("vgui/ttt/marker_vision/melonmine")
+    local materialMelonmine = Material("vgui/ttt/marker_vision/melonmine")
 
-	-- handle looking at C4
-	hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDMelonmine", function(tData)
-		local client = LocalPlayer()
-		local ent = tData:GetEntity()
+    -- handle looking at C4
+    hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDMelonmine", function(tData)
+        local client = LocalPlayer()
+        local ent = tData:GetEntity()
 
-		if not client:IsTerror() or not IsValid(ent) or tData:GetEntityDistance() > 100 or ent:GetClass() ~= "ttt_melonmine"
-			or client:GetTeam() ~= ent:GetOriginator():GetTeam()
-		then return end
+        if
+            not client:IsTerror()
+            or not IsValid(ent)
+            or tData:GetEntityDistance() > 100
+            or ent:GetClass() ~= "ttt_melonmine"
+            or client:GetTeam() ~= ent:GetOriginator():GetTeam()
+        then
+            return
+        end
 
-		-- enable targetID rendering
-		tData:EnableText()
-		tData:EnableOutline()
-		tData:SetOutlineColor(client:GetRoleColor())
+        -- enable targetID rendering
+        tData:EnableText()
+        tData:EnableOutline()
+        tData:SetOutlineColor(client:GetRoleColor())
 
-		tData:SetTitle(TryT(ent.PrintName))
+        tData:SetTitle(TryT(ent.PrintName))
 
-		if ent:GetOriginator() == client then
-			tData:SetKeyBinding("+use")
-			tData:SetSubtitle(ParT("target_pickup", {usekey = Key("+use", "USE")}))
-		else
-			tData:AddIcon(roles.DETECTIVE.iconMaterial)
-			tData:SetSubtitle(TryT("target_pickup_disabled"))
-		end
-	end)
+        if ent:GetOriginator() == client then
+            tData:SetKeyBinding("+use")
+            tData:SetSubtitle(ParT("target_pickup", { usekey = Key("+use", "USE") }))
+        else
+            tData:AddIcon(materialMelonmine)
+            tData:SetSubtitle(TryT("target_pickup_disabled"))
+        end
+    end)
 
-	hook.Add("TTT2RenderMarkerVisionInfo", "HUDDrawMarkerVisionMelonMine", function(mvData)
-		local client = LocalPlayer()
-		local ent = mvData:GetEntity()
-		local mvObject = mvData:GetMarkerVisionObject()
+    hook.Add("TTT2RenderMarkerVisionInfo", "HUDDrawMarkerVisionMelonMine", function(mvData)
+        local client = LocalPlayer()
+        local ent = mvData:GetEntity()
+        local mvObject = mvData:GetMarkerVisionObject()
 
-		if not client:IsTerror() or not mvObject:IsObjectFor(ent, "melon_owner") then return end
+        if not client:IsTerror() or not mvObject:IsObjectFor(ent, "melon_owner") then
+            return
+        end
 
-		local originator = ent:GetOriginator()
-		local nick = IsValid(originator) and originator:Nick() or "---"
+        local originator = ent:GetOriginator()
+        local nick = IsValid(originator) and originator:Nick() or "---"
 
-		local distance = math.Round(util.HammerUnitsToMeters(mvData:GetEntityDistance()), 1)
+        local distance = math.Round(util.HammerUnitsToMeters(mvData:GetEntityDistance()), 1)
 
-		mvData:EnableText()
+        mvData:EnableText()
 
-		mvData:AddIcon(materialMelonmine)
-		mvData:SetTitle(TryT(ent.PrintName))
+        mvData:AddIcon(materialMelonmine)
+        mvData:SetTitle(TryT(ent.PrintName))
 
-		mvData:AddDescriptionLine(ParT("marker_vision_owner", {owner = nick}))
-		mvData:AddDescriptionLine(ParT("marker_vision_distance", {distance = distance}))
+        mvData:AddDescriptionLine(ParT("marker_vision_owner", { owner = nick }))
+        mvData:AddDescriptionLine(ParT("marker_vision_distance", { distance = distance }))
 
-		mvData:AddDescriptionLine(TryT(mvObject:GetVisibleForTranslationKey()), COLOR_SLATEGRAY)
-	end)
+        mvData:AddDescriptionLine(TryT(mvObject:GetVisibleForTranslationKey()), COLOR_SLATEGRAY)
+    end)
 end
--- gamemodes/terrortown/entities/weapons/weapon_ttt_melonmine.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_melonmine.lua
@@ -1,41 +1,40 @@
 if SERVER then
-	AddCSLuaFile()
+    AddCSLuaFile()
 
-	resource.AddFile("materials/vgui/ttt/icon_melonmine.png")
+    resource.AddFile("materials/vgui/ttt/icon_melonmine.png")
 
-	util.PrecacheSound("weapons/c4/c4_beep1.wav")
-	util.PrecacheSound("weapons/c4/c4_plant.wav")
+    util.PrecacheSound("weapons/c4/c4_beep1.wav")
+    util.PrecacheSound("weapons/c4/c4_plant.wav")
 end
 
-DEFINE_BASECLASS "weapon_tttbase"
+DEFINE_BASECLASS("weapon_tttbase")
 
 SWEP.HoldType = "slam"
 
 if CLIENT then
-	SWEP.PrintName = "weapon_melonmine_name"
-	SWEP.Slot = 6
+    SWEP.PrintName = "weapon_melonmine_name"
+    SWEP.Slot = 6
 
-	SWEP.EquipMenuData = {
-		type = "item_weapon",
-		desc = "weapon_melonmine_desc"
-	}
+    SWEP.ViewModelFOV = 70
+    SWEP.ViewModelFlip = false
 
-	SWEP.Icon = "vgui/ttt/icon_melonmine.png"
+    SWEP.UseHands = true
+    SWEP.ShowDefaultViewModel = false
+    SWEP.ShowDefaultWorldModel = false
+
+    SWEP.EquipMenuData = {
+        type = "item_weapon",
+        desc = "weapon_melonmine_desc",
+    }
+
+    SWEP.Icon = "vgui/ttt/icon_melonmine.png"
 end
 
 SWEP.Base = "weapon_tttbase"
 
-SWEP.NextPlant = 0
-
-SWEP.ViewModelFOV = 70
-SWEP.ViewModelFlip = false
 SWEP.Kind = WEAPON_EQUIP
-SWEP.CanBuy = {ROLE_TRAITOR}
+SWEP.CanBuy = { ROLE_TRAITOR }
 SWEP.LimitedStock = true
-
-SWEP.UseHands = true
-SWEP.ShowDefaultViewModel = false
-SWEP.ShowDefaultWorldModel = false
 
 SWEP.Spawnable = false
 SWEP.AdminSpawnable = false
@@ -56,57 +55,64 @@ SWEP.Primary.Ammo = "slam"
 SWEP.Cat = "Explosives"
 
 function SWEP:PrimaryAttack()
-	if SERVER and self:CanPrimaryAttack() then
-		local melon = ents.Create("ttt_melonmine")
+    if SERVER and self:CanPrimaryAttack() then
+        local melon = ents.Create("ttt_melonmine")
 
-		if melon:StickEntity(self:GetOwner(), Angle(-90, 0, 180)) then
-			melon.fingerprints = self.fingerprints
+        if melon:StickEntity(self:GetOwner(), Angle(-90, 0, 180)) then
+            melon.fingerprints = self.fingerprints
 
-			self:Remove()
-		end
-	end
+            self:Remove()
+        end
+    end
 end
 
 function SWEP:OnRemove()
-	if CLIENT and IsValid(self:GetOwner()) and self:GetOwner() == LocalPlayer() and self:GetOwner():IsTerror() then
-		RunConsoleCommand("lastinv")
-	end
+    if
+        CLIENT
+        and IsValid(self:GetOwner())
+        and self:GetOwner() == LocalPlayer()
+        and self:GetOwner():IsTerror()
+    then
+        RunConsoleCommand("lastinv")
+    end
 end
 
 if CLIENT then
-	function SWEP:Initialize()
-		self:AddTTT2HUDHelp("melonmine_help_pri")
+    function SWEP:Initialize()
+        self:AddTTT2HUDHelp("melonmine_help_pri")
 
-		self:AddCustomViewModel("vmodel", {
-			type = "Model",
-			model = "models/props_junk/watermelon01.mdl",
-			bone = "ValveBiped.Bip01_R_Finger2",
-			rel = "",
-			pos = Vector(1.557, 4.9, 0),
-			angle = Angle(0, 0, 0),
-			size = Vector(0.625, 0.625, 0.625),
-			color = Color(255, 175, 0, 255),
-			surpresslightning = false,
-			material = "",
-			skin = 0,
-			bodygroup = {}
-		})
+        self.BaseClass.Initialize(self)
+    end
 
-		self:AddCustomWorldModel("wmodel", {
-			type = "Model",
-			model = "models/props_junk/watermelon01.mdl",
-			bone = "ValveBiped.Bip01_R_Hand",
-			rel = "",
-			pos = Vector(3, 6.752, 0),
-			angle = Angle(0, 0, 0),
-			size = Vector(0.625, 0.625, 0.625),
-			color = Color(255, 175, 0, 255),
-			surpresslightning = false,
-			material = "",
-			skin = 0,
-			bodygroup = {}
-		})
+    function SWEP:InitializeCustomModels()
+        self:AddCustomViewModel("vmodel", {
+            type = "Model",
+            model = "models/props_junk/watermelon01.mdl",
+            bone = "ValveBiped.Bip01_R_Finger2",
+            rel = "",
+            pos = Vector(1.557, 4.9, 0),
+            angle = Angle(0, 0, 0),
+            size = Vector(0.625, 0.625, 0.625),
+            color = Color(255, 175, 0, 255),
+            surpresslightning = false,
+            material = "",
+            skin = 0,
+            bodygroup = {},
+        })
 
-		self.BaseClass.Initialize(self)
-	end
+        self:AddCustomWorldModel("wmodel", {
+            type = "Model",
+            model = "models/props_junk/watermelon01.mdl",
+            bone = "ValveBiped.Bip01_R_Hand",
+            rel = "",
+            pos = Vector(3, 6.752, 0),
+            angle = Angle(0, 0, 0),
+            size = Vector(0.625, 0.625, 0.625),
+            color = Color(255, 175, 0, 255),
+            surpresslightning = false,
+            material = "",
+            skin = 0,
+            bodygroup = {},
+        })
+    end
 end
